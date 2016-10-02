ad27f7de1e0f4afe606a341869a7ad226adc7d7c
Lancewiu <ranchwiu@gmail.com>
2016-10-01 23:16:02 -0400

    updated binds (again)

--- index.js
+++
@@ -1,6 +0,0 @@
-// declare the angular application first (module called "myApp")
-angular.module('myApp')
-  .controller('myAppController', function($scope) {
-    $scope.user = {};
-    $scope.user.firstName = 'Joe';
-  });
